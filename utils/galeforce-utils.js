c25808ef6937e56de092a08b6c88d11ed3418024
Leinier Perez <P_leinier@yahoo.com>
2022-02-28 13:27:55 -0500

    Add galeforce utils to be able to retrieve champion data and more
    'getLatestVersion' returns the latest league of legends version
    'getChampionsList' returns a list of champions with a brief summary
    'getChampionData' returns individual JSON files containing more champion data

---
+++ utils/galeforce-utils.js
@@ -0,0 +1,31 @@
+const GaleforceModule = require('galeforce');
+const galeforce = new GaleforceModule('./galeforce-config.yml');
+
+galeforceUtils = {
+  async getLatestVersion() {
+    return (await galeforce.lol.ddragon.versions().exec())[0];
+  },
+  async getChampionsList() {
+    const championList = (
+      await galeforce.lol.ddragon.champion
+        .list()
+        .version(await galeforceUtils.getLatestVersion())
+        .locale('en_US')
+        .exec()
+    ).data;
+    return Object.values(championList);
+  },
+  async getChampionData(championName) {
+    const championData = (
+      await galeforce.lol.ddragon.champion
+        .details()
+        .version(await galeforceUtils.getLatestVersion())
+        .locale('en_US')
+        .champion(championName)
+        .exec()
+    ).data;
+    return championData;
+  },
+};
+
+module.exports = galeforceUtils;

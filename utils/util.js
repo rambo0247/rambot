ed8db4f1de31311e8359446f81bf6b9f4736c188
Leinier Perez <P_leinier@yahoo.com>
2022-03-02 16:03:58 -0500

    Add general util functions to avoid repetition

---
+++ utils/util.js
@@ -0,0 +1,25 @@
+const htmlparser2 = require('htmlparser2');
+
+module.exports = {
+  parse(unparsedText) {
+    let parsedText = '';
+    const parser1 = new htmlparser2.Parser({
+      ontext(text) {
+        parsedText += text;
+      },
+      onclosetag(tagname) {
+        if (tagname === 'br') {
+          parsedText += `\n`;
+        }
+      },
+    });
+
+    parser1.write(unparsedText);
+    parser1.end();
+
+    return parsedText;
+  },
+  randomInArray(array) {
+    return array[Math.floor(Math.random() * array.length)];
+  },
+};

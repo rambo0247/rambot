0d0ff7c646b690b2700e5dd65bbd0adb8d0ac30b
Leinier Perez <P_leinier@yahoo.com>
2022-03-08 01:16:56 -0500

    Add image buffer to fields and handle displaying of images with buffer

--- structures/Question.js
+++ structures/Question.js
@@ -1,23 +1,41 @@
-const { MessageEmbed } = require('discord.js');
+const { MessageEmbed, MessageAttachment } = require('discord.js');
 
 module.exports = class Question {
-  constructor(title, description, imageUrl, answer, points) {
+  constructor(
+    title,
+    description,
+    imageUrl,
+    answer,
+    points,
+    imageBuffer = null
+  ) {
     this.title = title;
     this.description = description;
     this.imageUrl = imageUrl;
     this.answer = answer;
     this.points = points;
     this.isExpired = false;
+    this.imageBuffer = imageBuffer;
   }
 
   async sendQuestionMessage(interaction) {
     const questionEmbed = new MessageEmbed()
       .setTitle(this.title)
       .setColor('RANDOM');
-
+    let attachment;
     if (this.description) questionEmbed.setDescription(this.description);
     else if (this.imageUrl) questionEmbed.setImage(this.imageUrl);
-    await interaction.channel.send({ embeds: [questionEmbed] });
+    else if (this.imageBuffer) {
+      attachment = new MessageAttachment(
+        this.imageBuffer,
+        'loading-screen-art.jpg'
+      );
+      questionEmbed.setImage('attachment://loading-screen-art.jpg');
+    }
+    await interaction.channel.send({
+      embeds: [questionEmbed],
+      files: attachment ? [attachment] : null,
+    });
   }
 
   async sendWrongAnswerMessage(interaction) {

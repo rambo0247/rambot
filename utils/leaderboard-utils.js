fc513aa0e91d752ad4a31a844f0ae3d1694c37d5
Leinier Perez <P_leinier@yahoo.com>
2022-03-09 17:39:19 -0500

    Change leaderboard command to be able to view it for a desired game
    After this commit the leaderboard command allows users to type the name of the
    game they want to view the leaderboard for. This allows for more organized code
    by separating leaderboard display logic in 'leaderboard-utils'.

---
+++ utils/leaderboard-utils.js
@@ -0,0 +1,61 @@
+const schema = require('../models/leaderboard');
+const { paginator } = require('../utils/paginator');
+const Ascii = require('ascii-table');
+
+module.exports = {
+  async getWordleLeaderboard(interaction) {
+    const pages = [];
+    let pageMsg = '';
+    const usersData = await schema.find().sort({ ramboPoints: -1 });
+    let playersPerPage = 10;
+    for (let index = 0; index < usersData.length; index += 10) {
+      const currentPlayer = usersData.slice(index, playersPerPage);
+      playersPerPage += 10;
+      let rankNumber = index;
+      const Table = new Ascii('Wordle Leaderboard');
+      Table.setHeading(
+        'Rank',
+        'Rambo Points',
+        'Username',
+        'W/L',
+        'Avg Guesses',
+        'Invalid words'
+      );
+      for (const {
+        userName,
+        ramboPoints,
+        totalGames,
+        totalGuesses,
+        totalWins,
+        totalLosses,
+        totalInvalidWords,
+      } of currentPlayer) {
+        Table.addRow(
+          `${++rankNumber}`,
+          `${ramboPoints}`,
+          `${userName}`,
+          `${totalWins}/${totalLosses} (${Math.round(
+            (totalWins / totalGames) * 100
+          )}%)`,
+          `${(totalGuesses / totalGames).toFixed(2)}`,
+          `${totalInvalidWords}`
+        );
+      }
+      for (let column = 0; column < 6; column++) {
+        Table.setAlign(column, Table.__nameAlign);
+      }
+      Table.removeBorder();
+      Table.setBorder('|', '_', '.', '|');
+      pageMsg = Table.toString();
+      pages.push(pageMsg);
+    }
+
+    if (pages.length === 0) {
+      await interaction.reply(
+        `${target.user.username} has not played a game of wordle`
+      );
+    } else {
+      paginator(interaction, pages);
+    }
+  },
+};

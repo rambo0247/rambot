8893242fcc0e4508b5efb5cba66dde2b74c54cc1
Leinier Perez <P_leinier@yahoo.com>
2022-03-16 00:23:38 -0400

    Add utils for currency system

---
+++ utils/currency-system-utils.js
@@ -0,0 +1,20 @@
+module.exports = {
+  async addMoney(currencySystem, moneyAmount, userId, guildId, location) {
+    const result = await currencySystem.addMoney({
+      user: userId,
+      guild: guildId,
+      amount: moneyAmount,
+      wheretoPutMoney: location,
+    });
+    if (result.error) console.log('You cant add negative money');
+  },
+  async removeMoney(currencySystem, moneyAmount, userId, guildId, location) {
+    const result = await currencySystem.removeMoney({
+      user: userId,
+      guild: guildId,
+      amount: moneyAmount,
+      wheretoPutMoney: location,
+    });
+    if (result.error) console.log('You cant remove negitive money');
+  },
+};

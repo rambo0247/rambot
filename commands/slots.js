ffe2e8b97b041b069d3a202e0b174aa999a2eab3
Leinier Perez <P_leinier@yahoo.com>
2022-03-17 13:39:02 -0400

    Create slots command that allows users to play a slot machine game
    The slot command will cycle through all the options and pick three at random.
    All options are worth the same amount of currency. Once the slots game ends it
    will update the users balance with their winnings or loses.

---
+++ commands/slots.js
@@ -0,0 +1,171 @@
+const {
+  CommandInteraction,
+  MessageEmbed,
+  MessageAttachment,
+  Client,
+} = require('discord.js');
+const { slots } = require('../validation/EmojiCodes');
+const {
+  addMoney,
+  removeMoney,
+  getUserBalance,
+} = require('../utils/currency-system-utils');
+const { randomInArray } = require('../utils/util');
+const CurrencySystem = require('currency-system');
+const { sendGameEndMessage } = require('../utils/slots-utils');
+const playerIds = new Set();
+
+module.exports = {
+  name: 'slots',
+  description: 'Play a game of slots',
+  options: [
+    {
+      name: 'bet',
+      description: 'Amount of money to bet (minimum: 50)',
+      type: 'INTEGER',
+      required: true,
+    },
+  ],
+  /**
+   *
+   * @param {CommandInteraction} interaction
+   * @param {Client} client
+   * @param {CurrencySystem} currencySystem
+   */
+  async execute(interaction, client, currencySystem) {
+    const betAmount = interaction.options.getInteger('bet');
+    const user = interaction.user;
+    const guildId = interaction.guild.id;
+    if (playerIds.has(interaction.user.id)) {
+      return await interaction.reply({
+        content: 'A slots game is already running',
+        ephemeral: true,
+      });
+    }
+    playerIds.add(user.id);
+    let userBalance = await getUserBalance(currencySystem, user, guildId);
+    if (betAmount < 50)
+      return await interaction.reply({
+        content: 'Please enter a valid bet amount, minimum is 50',
+        ephemeral: true,
+      });
+    if (userBalance.wallet.toLocaleString() < betAmount) {
+      return await interaction.reply({
+        content: 'You do not have enough money in your wallet for this bet',
+        ephemeral: true,
+      });
+    }
+    const pairWinMultiplier = 3;
+    const tripleWinMultiplier = 12;
+    const animatedSlots = slots.animatedSlots;
+    const slotEmojis = [
+      slots.seven,
+      slots.apple,
+      slots.cherry,
+      slots.crown,
+      slots.dollarSign,
+      slots.lemon,
+      slots.watermelon,
+    ];
+    const slotOne = randomInArray(slotEmojis);
+    const slotTwo = randomInArray(slotEmojis);
+    const slotThree = randomInArray(slotEmojis);
+    const cyclingSlotsMsg = `${animatedSlots} ${animatedSlots} ${animatedSlots}`;
+    const oneSlotLockedMsg = `${slotOne} ${animatedSlots} ${animatedSlots}`;
+    const twoSlotsLockedMsg = `${slotOne} ${slotTwo} ${animatedSlots}`;
+    const threeSlotsLockedMsg = `${slotOne} ${slotTwo} ${slotThree}`;
+    const loseIcon = new MessageAttachment(
+      './assets/slots/lose-icon.png',
+      'lose-icon.png'
+    );
+    const pairWinIcon = new MessageAttachment(
+      './assets/slots/win-pair.png',
+      'win-pair.png'
+    );
+    const jackpotWinIcon = new MessageAttachment(
+      './assets/slots/jackpot.png',
+      'jackpot.png'
+    );
+
+    await interaction.deferReply();
+    await interaction.followUp({
+      content: cyclingSlotsMsg,
+    });
+
+    setTimeout(async () => {
+      await interaction.editReply({
+        content: oneSlotLockedMsg,
+      });
+    }, 1000 * 1);
+    setTimeout(async () => {
+      await interaction.editReply({
+        content: twoSlotsLockedMsg,
+      });
+    }, 1000 * 3);
+    setTimeout(async () => {
+      await interaction.editReply({
+        content: threeSlotsLockedMsg,
+      });
+      playerIds.delete(user.id);
+    }, 1000 * 5);
+
+    const gameEndEmbed = new MessageEmbed()
+      .setTitle(':slot_machine: SLOTS :slot_machine:')
+      .setAuthor({
+        name: `${interaction.user.username}`,
+        iconURL: `${interaction.user.displayAvatarURL()}`,
+      })
+      .setDescription(threeSlotsLockedMsg)
+      .setColor('GREEN');
+
+    if (slotTwo !== slotOne && slotTwo !== slotThree) {
+      const messageField = {
+        name: 'Loser',
+        value: 'You lost',
+      };
+      const payout = betAmount;
+      await removeMoney(currencySystem, payout, user, guildId, 'wallet');
+      userBalance = await getUserBalance(currencySystem, user, guildId);
+      sendGameEndMessage(
+        payout,
+        loseIcon,
+        gameEndEmbed,
+        interaction,
+        messageField,
+        userBalance
+      );
+    } else if (slotOne === slotTwo && slotOne === slotThree) {
+      const messageField = {
+        name: 'Jackpot',
+        value: 'You won',
+      };
+      const payout = betAmount * tripleWinMultiplier;
+      await addMoney(currencySystem, payout, user, guildId, 'wallet');
+      userBalance = await getUserBalance(currencySystem, user, guildId);
+      sendGameEndMessage(
+        payout,
+        jackpotWinIcon,
+        gameEndEmbed,
+        interaction,
+        messageField,
+        userBalance
+      );
+    } else {
+      const messageField = {
+        name: 'Pair',
+        value: 'You won',
+      };
+      const payout = betAmount * pairWinMultiplier;
+      await addMoney(currencySystem, payout, user, guildId, 'wallet');
+      userBalance = await getUserBalance(currencySystem, user, guildId);
+      sendGameEndMessage(
+        payout,
+        pairWinIcon,
+        gameEndEmbed,
+        interaction,
+        messageField,
+        userBalance
+      );
+    }
+  },
+};

9359a8ff6009fa76a7f78d5f8d5238f69c6f8fd2
Leinier Perez <P_leinier@yahoo.com>
2022-03-17 14:31:18 -0400

    Create deposit command to allow users to move money from wallet to bank

---
+++ commands/deposit.js
@@ -0,0 +1,70 @@
+const {
+  CommandInteraction,
+  Client,
+  MessageEmbed,
+  MessageAttachment,
+} = require('discord.js');
+const CurrencySystem = require('currency-system');
+const { rCoin } = require('../validation/EmojiCodes');
+
+module.exports = {
+  name: 'deposit',
+  description: 'Deposits R-Coins from your wallet into your bank',
+  options: [
+    {
+      name: 'amount',
+      description: 'Amount of R-Coins to deposit',
+      type: 'INTEGER',
+      required: true,
+    },
+  ],
+  /**
+   *
+   * @param {CommandInteraction} interaction
+   * @param {Client} client
+   * @param {CurrencySystem} currencySystem
+   */
+  async execute(interaction, client, currencySystem) {
+    const amountToDeposit = interaction.options.getInteger('amount');
+    const user = interaction.user;
+    const result = await currencySystem.deposite({
+      user: user,
+      guild: interaction.guild.id,
+      amount: amountToDeposit,
+    });
+    const bankingIcon = new MessageAttachment(
+      './assets/economy/banking.png',
+      'banking.png'
+    );
+    const date = new Date(Date.now());
+    const depositEmbed = new MessageEmbed()
+      .setTitle('Deposit')
+      .setColor('RANDOM')
+      .setAuthor({
+        name: user.username,
+        iconURL: `${user.displayAvatarURL()}`,
+      })
+      .setThumbnail('attachment://banking.png')
+      .setFooter({
+        text: `Deposit receipt • ${date.getMonth()}/${date.getDay()}/${date.getFullYear()}`,
+      });
+    let embedDescription;
+    if (result.error) {
+      if (result.type === 'negative-money')
+        embedDescription = "You can't withdraw negative R-Coins";
+      if (result.type === 'low-money')
+        embedDescription = "You don't have enough R-Coins in bank.";
+      if (result.type === 'no-money')
+        embedDescription = "You don't have any R-Coins to withdraw";
+      if (result.type === 'bank-full')
+        embedDescription = 'Your bank limit has been reached';
+    } else {
+      embedDescription = `Amount Deposited: ${amountToDeposit} ${rCoin}\nWallet: ${result.rawData.wallet} ${rCoin}\nBank: ${result.rawData.bank} ${rCoin}`;
+    }
+    depositEmbed.setDescription(embedDescription);
+    await interaction.reply({
+      embeds: [depositEmbed],
+      files: [bankingIcon],
+    });
+  },
+};

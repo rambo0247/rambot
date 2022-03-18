ce56e6e4a30d4d17af7b4b6919bb380177d57ca0
Leinier Perez <P_leinier@yahoo.com>
2022-03-18 02:09:52 -0400

    Fix bug where it was not getting the correct month number

--- utils/util.js
+++ utils/util.js
@@ -55,7 +55,7 @@ module.exports = {
   getDate() {
     // Format: monthNumber/day/year
     const date = new Date(Date.now());
-    const monthNumber = ('0' + date.getMonth() + 1).slice(-2);
+    const monthNumber = ('0' + (date.getMonth() + 1)).slice(-2);
     const day = ('0' + date.getDate()).slice(-2);
     return `${monthNumber}/${day}/${date.getFullYear()}`;
   },

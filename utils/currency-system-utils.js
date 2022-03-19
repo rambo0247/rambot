746c920398fed1e769470e52e4673bb751d6a865
Leinier Perez <P_leinier@yahoo.com>
2022-03-19 16:09:46 -0400

    Add utility function for currency system to get users command cooldowns

--- utils/currency-system-utils.js
+++ utils/currency-system-utils.js
@@ -1,3 +1,4 @@
+const { parseSeconds } = require('./util');
 module.exports = {
   async addMoney(currencySystem, moneyAmount, userId, guildId, location) {
     const result = await currencySystem.addMoney({
@@ -25,5 +26,42 @@ module.exports = {
       guild: guildId,
     });
     return balance;
+  },
+  async getUserCommandsCooldowns(currencySystem, interaction) {
+    const userId = interaction.user.id;
+    const guildId = interaction.guild.id;
+    const result = await currencySystem.info(userId, guildId);
+    const data = await currencySystem.findUser({
+      user: userId,
+      guild: guildId,
+    });
+    const robCommandCooldown = 3600;
+    const coinflipCommandCooldown = 60;
+    const robTimeLeft = parseSeconds(
+      robCommandCooldown - (Date.now() - data.lastRob) / 1000
+    );
+    const coinflipTimeLeft = parseSeconds(
+      coinflipCommandCooldown - (Date.now() - data.lastGamble) / 1000
+    );
+    result.info.push(['Rob', { timeLeft: robTimeLeft }]);
+    result.info.push(['Coinflip', { timeLeft: coinflipTimeLeft }]);
+    let commandsCooldownInfo = '';
+    const commandsToShow = {
+      daily: 'Daily',
+      weekly: 'Weekly',
+      work: 'Work',
+      rob: 'Rob',
+      coinflip: 'Coinflip',
+    };
+    for (const [key, value] of result.info) {
+      if (key.toLowerCase() in commandsToShow) {
+        if (value.used || value.timeLeft == '0 Seconds') {
+          commandsCooldownInfo += `**${key}** - ready\n`;
+        } else {
+          commandsCooldownInfo += `**${key}** - ${value.timeLeft}\n`;
+        }
+      }
+    }
+    return commandsCooldownInfo;
   },
 };

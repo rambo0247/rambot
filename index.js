40fb8843f0edc25d9161f793099683099749097c
Leinier Perez <P_leinier@yahoo.com>
2022-03-21 08:25:17 -0400

    Update intents

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const client = new Client({
     Intents.FLAGS.GUILD_INTEGRATIONS,
     Intents.FLAGS.GUILD_MESSAGES,
     Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+    Intents.FLAGS.GUILD_PRESENCES,
+    Intents.FLAGS.GUILD_SCHEDULED_EVENTS,
   ],
 });
 
